d48586fdfa1810fdbac77f4f15a9adf9d4989473
paul <paulbrowne2@gmail.com>
2022-03-08 05:39:58 +0000

    Set up file structure and basic stylings in index.css

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,23 @@
+import {
+  Navbar,
+  Welcome,
+  Services,
+  Transactions,
+  Footer,
+} from "./components";
+
 const App = () => {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    <div className="min-h-screen">
+      <div className="gradient-bg-welcome">
+        <Navbar />
+        <Welcome />
+      </div>
+      <Services />
+      <Transactions />
+      <Footer />
     </div>
   );
-}
+};
 
 export default App;
