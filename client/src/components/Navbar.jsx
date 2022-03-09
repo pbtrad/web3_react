eda2622dfadee039d877589d2a69c556189d8c0f
paul <paulbrowne2@gmail.com>
2022-03-09 17:01:27 +0000

    Finished footer and nav icon

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { HiMenuAlt4 } from "react-icons/hi";
 import { AiOutlineClose } from "react-icons/ai";
 
-import logo from "../../images/logo.png";
+import logo from "../../images/celtic.png";
 
 const NavBarItem = ({ title, classprops }) => {
   return <li className={`mx-4 cursor-pointer ${classprops}`}>{title}</li>;
