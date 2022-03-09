2c98594e5064eaa7d2d148680954cac2a1214d03
paul <paulbrowne2@gmail.com>
2022-03-09 14:38:32 +0000

    Added services card section

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -3,6 +3,7 @@ import { AiFillPlayCircle } from "react-icons/ai";
 import { SiEthereum } from "react-icons/si";
 import { BsInfoCircle } from "react-icons/bs";
 import { TransactionContext } from "../context/TransactionContext";
+import { shortenAddress } from "../utils/shortenAddress";
 import { Loader } from ".";
 
 const companyCommonStyles =
